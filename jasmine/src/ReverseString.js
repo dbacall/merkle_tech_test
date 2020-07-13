ad449024817f37d18245b4aeb357a98fd9af9b81
dbacall <dbacall@hotmail.co.uk>
2020-07-13 15:45:14 +0100

    refactor: clean up reverseString

--- jasmine/src/ReverseString.js
+++ jasmine/src/ReverseString.js
@@ -1,7 +1,6 @@
 var reverseString = (string) => {
   var stringWithLastLetterRemoved = string.slice(0, string.length - 1);
   var lastLetter = string.slice(string.length - 1, string.length);
-  console.log(lastLetter);
   if (string.length > 0) {
     return lastLetter + reverseString(stringWithLastLetterRemoved);
   } else {
