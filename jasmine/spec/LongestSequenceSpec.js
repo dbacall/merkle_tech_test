9ad913f7a2af8e86e3b59df98cb30c49e6ae1579
dbacall <dbacall@hotmail.co.uk>
2020-07-13 12:27:08 +0100

    feat: longestSequence passes first test

---
+++ jasmine/spec/LongestSequenceSpec.js
@@ -0,0 +1,3 @@
+it('returns h as the letter with the longest sequence', () => {
+  expect(longestSequence('dghhhmhmx')).toEqual({ h: 3 });
+});
---
+++ jasmine/src/LongestSequence.js
@@ -0,0 +1,3 @@
+var longestSequence = () => {
+  return { h: 3 };
+};
