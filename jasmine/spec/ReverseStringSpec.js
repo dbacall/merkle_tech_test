83651c00b34bf1aa25d747263b3c7657827d2ff6
dbacall <dbacall@hotmail.co.uk>
2020-07-13 15:44:09 +0100

    feat: reverseString recursively returns a reversed string

--- jasmine/spec/ReverseStringSpec.js
+++ jasmine/spec/ReverseStringSpec.js
@@ -1,3 +1,15 @@
 it('returns bcba', () => {
   expect(reverseString('abcb')).toEqual('bcba');
 });
+
+it('returns tset', () => {
+  expect(reverseString('test')).toEqual('tset');
+});
+
+it('returns racecar', () => {
+  expect(reverseString('racecar')).toEqual('racecar');
+});
+
+it('returns an empty string', () => {
+  expect(reverseString('')).toEqual('');
+});
--- jasmine/src/ReverseString.js
+++ jasmine/src/ReverseString.js
@@ -1,3 +1,10 @@
 var reverseString = (string) => {
-  return 'bcba';
+  var stringWithLastLetterRemoved = string.slice(0, string.length - 1);
+  var lastLetter = string.slice(string.length - 1, string.length);
+  console.log(lastLetter);
+  if (string.length > 0) {
+    return lastLetter + reverseString(stringWithLastLetterRemoved);
+  } else {
+    return lastLetter;
+  }
 };
