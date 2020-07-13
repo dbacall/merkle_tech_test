67b9388bbbfc3f68618c3a2a082fc5aa70019ca1
dbacall <dbacall@hotmail.co.uk>
2020-07-13 14:40:18 +0100

    feat: secondLowest works for arrays with no repeats

--- jasmine/spec/SecondLowestSpec.js
+++ jasmine/spec/SecondLowestSpec.js
@@ -1,3 +1,11 @@
 it('returns 1', () => {
   expect(secondLowest([4, 3, 1, 1, 2])).toEqual(1);
 });
+
+it('returns 2', () => {
+  expect(secondLowest([4, 3, 1, 1, 2, 2])).toEqual(2);
+});
+
+it('returns 2', () => {
+  expect(secondLowest([4, 3, 1, 2])).toEqual(2);
+});
--- jasmine/src/SecondLowest.js
+++ jasmine/src/SecondLowest.js
@@ -3,11 +3,47 @@ var secondLowest = (numbers) => {
   numbers.forEach((num) => {
     frequencies[num] ? (frequencies[num] += 1) : (frequencies[num] = 1);
   });
+
   var min = Math.min.apply(null, Object.values(frequencies).filter(Boolean));
+
   var secondMin = Math.min.apply(
     null,
     Object.values(frequencies).filter((n) => n != min)
   );
+
   var indexOfSecondMin = Object.values(frequencies).indexOf(secondMin);
-  return parseInt(Object.keys(frequencies)[indexOfSecondMin]);
+
+  var lastIndexOfSecondMin = Object.values(frequencies).lastIndexOf(secondMin);
+
+  var arrayOfSecondMins = Object.keys(frequencies)
+    .splice(indexOfSecondMin, lastIndexOfSecondMin + 1)
+    .map((num) => {
+      return parseInt(num);
+    });
+
+  var minKey = Math.min.apply(null, arrayOfSecondMins.filter(Boolean));
+
+  var secondMinKey = Math.min.apply(
+    null,
+    arrayOfSecondMins.filter((n) => n != minKey)
+  );
+
+  var indexOfSecondMinKey = arrayOfSecondMins.indexOf(secondMinKey);
+
+  var minWithNoRepeats = Math.min.apply(null, numbers.filter(Boolean));
+
+  var secondMinWithNoRepeats = Math.min.apply(
+    null,
+    numbers.filter((n) => n != minWithNoRepeats)
+  );
+
+  var indexOfSecondMinWithNoRepeats = numbers.indexOf(secondMinWithNoRepeats);
+
+  if (arrayOfSecondMins.length === 1) {
+    return parseInt(Object.keys(frequencies)[indexOfSecondMin]);
+  } else if (arrayOfSecondMins.length > 1) {
+    return parseInt(Object.keys(frequencies)[indexOfSecondMinKey]);
+  } else {
+    return numbers[indexOfSecondMinWithNoRepeats];
+  }
 };
