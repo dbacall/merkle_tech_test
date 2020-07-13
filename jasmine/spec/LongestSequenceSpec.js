89ba97e099a50961cb3c7fabb6b7fc35d46c60bf
dbacall <dbacall@hotmail.co.uk>
2020-07-13 13:26:59 +0100

    feat: longestSequence now works for strings with repeating letter sequences

--- jasmine/spec/LongestSequenceSpec.js
+++ jasmine/spec/LongestSequenceSpec.js
@@ -1,3 +1,7 @@
 it('returns h as the letter with the longest sequence', () => {
   expect(longestSequence('dghhhmhmx')).toEqual({ h: 3 });
 });
+
+it('returns k as the letter with the longest sequence', () => {
+  expect(longestSequence('dhkkhhKKKt')).toEqual({ k: 3 });
+});
--- jasmine/src/LongestSequence.js
+++ jasmine/src/LongestSequence.js
@@ -1,3 +1,22 @@
-var longestSequence = () => {
-  return { h: 3 };
+var longestSequence = (letters) => {
+  var currentSequence = {};
+  var letterSequences = [];
+  letters
+    .toLowerCase()
+    .split('')
+    .forEach((letter, i) => {
+      if (i === 0) {
+        currentSequence[letter] = 1;
+      } else if (!currentSequence[letter]) {
+        letterSequences.push(currentSequence);
+        currentSequence = {};
+        currentSequence[letter] = 1;
+      } else {
+        currentSequence[letter] += 1;
+      }
+    });
+  console.log(letterSequences);
+  return letterSequences.reduce((a, b) => {
+    return Object.values(a)[0] > Object.values(b)[0] ? a : b;
+  });
 };
